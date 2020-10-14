1e90897420f65443759d46fbb64ccce45d846364
fiferortiz <pedrofelipeortiz@gmail.com>
2020-10-14 08:17:02 -0500

    primer comiit

---
+++ js/getCategorias.js
@@ -0,0 +1,18 @@
+
+export function getCategories() {
+    alert('funciona el import')
+    const questionsQuantity = document.getElementById('questions-number').value
+    fetch(`https://opentdb.com/api_category.php`)
+        .then(response => response.json())
+        .then(data => Categories(data.trivia_categories))  
+}
+
+function Categories(data){
+    const categories = document.getElementById('questions-category')
+    data.forEach(data=>{
+        const Category = `<option id"${data.id}" value="${data.id}">${data.name}</option>`
+        categories.innerHTML += Category;
+    })
+    console.log(data)
+}
+
